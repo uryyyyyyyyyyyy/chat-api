8e75d4f9062863d8635c168de329084ad37537aa
樋上優希 <jyugorou@higamiyuukinoMacBook-Pro.local>
2025-06-16 13:23:46 +0900

    Add build/web for Netlify deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a2f6c98c52e08b767ac35dee89ec596b",
+const RESOURCES = {"flutter_bootstrap.js": "cb3cf16f6f5831ff9fa5455facd6cf32",
 "version.json": "5418904cc6bcc54da70ca3c1f9a2d865",
 "index.html": "87073d69a7dcf86f5e25572899a780c5",
 "/": "87073d69a7dcf86f5e25572899a780c5",
